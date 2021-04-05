e862de71eb3d913bfec8ba885fa8bd27d2a40261
malicious <38064672+malicious@users.noreply.github.com>
2021-04-05 14:30:01 -0700

    [tasks-v2] Basic /report page, based on old template
    Doesn't show actual status, though.

--- tasks_v2/report.py
+++ tasks_v2/report.py
@@ -1,7 +1,25 @@
+import json
 from typing import Optional
 
-from tasks.time_scope import TimeScope
-from tasks_v2.models import Task
+from flask import render_template
+
+from tasks.time_scope import TimeScope, TimeScopeUtils
+from tasks_v2.models import Task, TaskLinkage
+
+
+def to_details_html(t: Task):
+    as_text = json.dumps(t.to_json_dict(), indent=4, ensure_ascii=False)
+    return as_text
+
+
+def to_summary_html(t: Task, ref_scope: Optional[TimeScope] = None) -> str:
+    response_html = ""
+    response_html += f'\n<span class="desc">{t.desc}</span>'
+    response_html += f'\n<span class="task-id"><a href="/task-v2/{t.task_id}">#{t.task_id}</a></span>'
+
+    return '<summary class="task">' + \
+           response_html + '\n' + \
+           '</summary>'
 
 
 def report_one_task(task_id):
@@ -14,5 +32,57 @@ def report_one_task(task_id):
     return task.to_json_dict()
 
 
+def generate_tasks_by_scope(page_scope: Optional[TimeScope]):
+    # Identify the scopes that we care about
+    page_scopes_all = []
+
+    if page_scope is not None:
+        page_scope_linkages = TaskLinkage.query \
+            .filter(TaskLinkage.time_scope_id.like(page_scope + "%")) \
+            .order_by(TaskLinkage.time_scope_id) \
+            .all()
+        page_scopes_all = [
+            *TimeScopeUtils.enclosing_scope(page_scope, recurse=True),
+            page_scope,
+            *[tl.time_scope_id for tl in page_scope_linkages],
+        ]
+    else:
+        page_scope_linkages = TaskLinkage.query \
+            .order_by(TaskLinkage.time_scope_id) \
+            .all()
+        page_scopes_all = [
+            tl.time_scope_id for tl in page_scope_linkages
+        ]
+
+    # Identify all tasks within those scopes
+    tasks_by_scope = {}
+
+    for scope in page_scopes_all:
+        tasks = Task.query \
+            .join(TaskLinkage, Task.task_id == TaskLinkage.task_id) \
+            .filter(TaskLinkage.time_scope_id == scope) \
+            .order_by(TaskLinkage.time_scope_id, Task.category) \
+            .all()
+        tasks_by_scope[TimeScope(scope)] = tasks
+
+    return tasks_by_scope
+
+
 def report_tasks(page_scope: Optional[TimeScope]):
-    return f"page_scope: {page_scope}"
+    render_kwargs = {}
+
+    # Identify all tasks within those scopes
+    render_kwargs['tasks_by_scope'] = generate_tasks_by_scope(page_scope)
+
+    # If there are previous/next links, add them
+    if page_scope:
+        prev_scope = TimeScopeUtils.prev_scope(page_scope)
+        render_kwargs['prev_scope'] = f'<a href="/report-tasks-v2?scope={prev_scope}">{prev_scope}</a>'
+        next_scope = TimeScopeUtils.next_scope(page_scope)
+        render_kwargs['next_scope'] = f'<a href="/report-tasks-v2?scope={next_scope}">{next_scope}</a>'
+
+    # Tell template about how to format Tasks
+    render_kwargs['to_details_html'] = to_details_html
+    render_kwargs['to_summary_html'] = to_summary_html
+
+    return render_template('task.html', **render_kwargs)
--- tests/tasks_v2_report_test.py
+++ tests/tasks_v2_report_test.py
@@ -1,5 +1,8 @@
-from tasks_v2.models import Task
-from tasks_v2.report import report_one_task
+import itertools
+
+from tasks.time_scope import TimeScope
+from tasks_v2.models import Task, TaskLinkage
+from tasks_v2.report import generate_tasks_by_scope, report_one_task
 
 
 def test_report_one_empty():
@@ -18,3 +21,71 @@ def test_report_one_simple(session):
     assert json_dict
     assert "task_id" in json_dict
     assert json_dict["task_id"] == t2.task_id
+
+
+def test_report_empty():
+    tasks_by_scope = generate_tasks_by_scope(page_scope=None)
+
+    assert tasks_by_scope is not None
+    assert not tasks_by_scope
+
+
+def test_report_simple(session):
+    t2 = Task(desc="reported task test")
+    session.add(t2)
+    session.commit()
+
+    TEST_SCOPE = "2021-ww14.1"
+    tl = TaskLinkage(task_id=t2.task_id, time_scope_id=TEST_SCOPE)
+    session.add(tl)
+
+    tasks_by_scope = generate_tasks_by_scope(page_scope=None)
+    assert tasks_by_scope
+    assert TEST_SCOPE in tasks_by_scope
+    assert len(tasks_by_scope[TEST_SCOPE]) == 1
+
+
+def test_report_simple_2(session):
+    t2 = Task(desc="reported task test (multi-scope)")
+    session.add(t2)
+    session.commit()
+
+    TEST_SCOPE_MONDAY = "2021-ww14.1"
+    tl_monday = TaskLinkage(task_id=t2.task_id, time_scope_id=TEST_SCOPE_MONDAY)
+    session.add(tl_monday)
+
+    TEST_SCOPE_TUESDAY = "2021-ww14.2"
+    tl_tuesday = TaskLinkage(task_id=t2.task_id, time_scope_id=TEST_SCOPE_TUESDAY)
+    session.add(tl_tuesday)
+
+    session.commit()
+
+    tasks_by_scope = generate_tasks_by_scope(page_scope=None)
+    assert len(tasks_by_scope.items()) == 2
+
+    assert TEST_SCOPE_MONDAY in tasks_by_scope
+    assert len(tasks_by_scope[TEST_SCOPE_MONDAY]) == 1
+    assert TEST_SCOPE_TUESDAY in tasks_by_scope
+    assert len(tasks_by_scope[TEST_SCOPE_TUESDAY]) == 1
+
+
+def test_report_week(session):
+    t2 = Task(desc="reported task test (multi-scope)")
+    session.add(t2)
+    session.commit()
+
+    TEST_SCOPE_MONDAY = "2021-ww14.1"
+    tl_monday = TaskLinkage(task_id=t2.task_id, time_scope_id=TEST_SCOPE_MONDAY)
+    session.add(tl_monday)
+
+    TEST_SCOPE_TUESDAY = "2021-ww14.2"
+    tl_tuesday = TaskLinkage(task_id=t2.task_id, time_scope_id=TEST_SCOPE_TUESDAY)
+    session.add(tl_tuesday)
+
+    session.commit()
+
+    # Same task will show up in multiple scopes
+    tasks_by_scope = generate_tasks_by_scope(page_scope=TimeScope("2021-ww14"))
+
+    flatten_unique = lambda x: set(itertools.chain.from_iterable(x))
+    assert len(flatten_unique(tasks_by_scope.values())) == 1
