6e5d2ca40cce1004d697e008b2ee9d2065b17271
malicious <38064672+malicious@users.noreply.github.com>
2021-05-01 17:31:01 +0000

    [import-notes] Add short-circuit duplicate check
    Use desc + short_desc; faster based on one dataset

--- notes/add.py
+++ notes/add.py
@@ -32,43 +32,80 @@ def _add_domains_for(note_id, domain_str: str, session, do_commit: bool = True):
         session.commit()
 
 
+def _find_existing_note(all_fields, fields_allow_list):
+    unique_args = {}
+    for field in fields_allow_list:
+        if field in all_fields and all_fields[field] is not None:
+            unique_args[field] = all_fields[field]
+        else:
+            # This helps with some kind of search error in the below `.filter_by` call,
+            # specifically when a datetime (created_at?) doesn't exist, then a later
+            # json.dumps() call fails because it doesn't know what to do with default-created datetimes.
+            #
+            # I think.
+            unique_args[field] = None
+
+    n = Note.query \
+        .filter_by(**unique_args) \
+        .one_or_none()
+    # TODO: multiple isn't _that_ exceptional, handle the case without exceptions
+    # - replace one_or_none() with all() and length checks (maybe)
+    return n
+
+
+
 def _add_note(session, domains: str, **kwargs) -> Optional[Note]:
     # Skip blank rows.
     # Happens sometimes when adding blank rows in a CSV editor
     if len(kwargs) == 0:
         return None
 
-    unique_args = {}
-    for field in ['source', 'type', 'sort_time', 'time_scope_id', 'short_desc', 'desc']:
-        if field in kwargs and kwargs[field]:
-            unique_args[field] = kwargs[field]
-        else:
-            unique_args[field] = None
-
     # Check for an existing note
     try:
-        n = Note.query \
-            .filter_by(**unique_args) \
-            .one_or_none()
+        # Do fast check for existing notes (sub-minimal set of unique fields)
+        n = _find_existing_note(kwargs, ['time_scope_id', 'short_desc'])
+
+        # Fast part: if nothing exists, just create a new note
+        if not n:
+            n = Note(**kwargs)
+            session.add(n)
+
+            try:
+                session.commit()
+            except StatementError as e:
+                print(f"Hit exception when parsing: {e}")
+                print(json.dumps(n.to_json(), indent=4))
+                session.rollback()
+                return None
+
+    # If fast check returned multiple notes, do a more thorough check
     except MultipleResultsFound as e:
-        print(e)
-        print(json.dumps(kwargs, indent=4))
-        return None
-
-    if not n:
-        # Otherwise just create a new note
-        n = Note(**kwargs)
-        session.add(n)
-
         try:
-            session.commit()
-        except StatementError as e:
-            print(f"Hit exception when parsing: {e}")
-            print(json.dumps(n.to_json(), indent=4))
-            session.rollback()
+            n = _find_existing_note(kwargs, ['source', 'type', 'sort_time', 'time_scope_id', 'short_desc', 'desc'])
+        except MultipleResultsFound as e:
+            # super-failure case: multiple entries exist, just skip it
+            print(e)
+            print(json.dumps(kwargs, indent=4))
             return None
 
-    # Add domains, note to database
+        # simpler case: exactly one note already exists
+        if n is not None:
+            pass
+
+        # final case: just create a new note
+        else:
+            n = Note(**kwargs)
+            session.add(n)
+
+            try:
+                session.commit()
+            except StatementError as e:
+                print(f"Hit exception when parsing: {e}")
+                print(json.dumps(n.to_json(), indent=4))
+                session.rollback()
+                return None
+
+    # Of all the fields we could care about, only domains are potentially updated.
     try:
         _add_domains_for(n.note_id, domains, session)
     except (ValueError, StatementError) as e:
