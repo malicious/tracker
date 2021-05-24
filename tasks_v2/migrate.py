f0b48b3eeb6494f3c3051122ba4838e2a66b93b4
malicious <38064672+malicious@users.noreply.github.com>
2021-05-24 16:25:41 +0000

    [DNM][tasks_v2-migrate] Works more-or-less
    - week objects aren't supported, what do we do?

--- tasks_v2/migrate.py
+++ tasks_v2/migrate.py
@@ -1,12 +1,213 @@
 from tasks_v1.models import Task as Task_v1
+
+from sqlalchemy.exc import IntegrityError, StatementError
+
+from tasks_v1.models import Task as Task_v1, TaskTimeScope
+from tasks_v1.time_scope import TimeScope, TimeScopeUtils
 from tasks_v2.models import Task as Task_v2, TaskLinkage
 
 MIGRATION_BATCH_SIZE = 50
 
 
+def _get_scopes(t: Task_v1):
+    def trim_quarter_scopes(scope_str):
+        maybe_quarter_scope = TimeScope(scope_str)
+        if maybe_quarter_scope.type != TimeScope.Type.quarter:
+            return scope_str
+
+        # Convert "quarter" scopes into their first day
+        scope = maybe_quarter_scope.start.strftime("%G-ww%V.%u")
+        print(f"INFO: task #{t.task_id} has quarter scope {scope_str}, downsizing to {scope}")
+        return scope
+
+    tts_query = TaskTimeScope.query \
+        .filter_by(task_id=t.task_id) \
+        .order_by(TaskTimeScope.time_scope_id)
+
+    return [trim_quarter_scopes(tts.time_scope_id) for tts in tts_query.all()]
+
+
+def _construct_linkages(t1: Task_v1, t2: Task_v2):
+    """
+    Turn TaskTimeScopes and child tasks into TaskLinkages
+    """
+    draft_linkages = {}
+
+    # Add all scopes attached to Task_v1
+    t1_scopes = _get_scopes(t1)
+    for index, scope in enumerate(t1_scopes):
+        date_scope = datetime.strptime(scope, '%G-ww%V.%u').date()
+        tl = TaskLinkage(task_id=t2.task_id, time_scope_id=date_scope)
+        tl.created_at = datetime.now()
+        draft_linkages[scope] = tl
+    del index, scope
+
+    # Put resolution info into final scope
+    tl_final = draft_linkages[t1_scopes[-1]]
+    tl_final.resolution = t1.resolution
+    tl_final.time_elapsed = t1.time_actual
+    del tl_final
+    del t1_scopes
+
+    # Case: unclear, gonna do our best
+    def parse_child(child_task: Task_v1):
+        child_task_scopes = _get_scopes(child_task)
+        if len(child_task_scopes) < 1:
+            raise ValueError(f"ERROR: Task #{child_task.task_id} has no associated scopes")
+
+        should_populate_first_scope = child_task_scopes[0] not in draft_linkages
+
+        # Create a new linkage for each new child scope
+        for index, scope in enumerate(child_task_scopes):
+            if not scope in draft_linkages:
+                date_scope = datetime.strptime(scope, '%G-ww%V.%u').date()
+                tl = TaskLinkage(task_id=t2.task_id, time_scope_id=date_scope)
+                tl.created_at = datetime.now()
+                draft_linkages[scope] = tl
+        del index, scope
+
+        # Then, dump all the child task info into the first associated scope
+        info_string = child_task.desc
+
+        if child_task.resolution != "info":
+            # Meld the resolution + desc into the TaskLinkage's resolution info
+            info_string = f"({child_task.resolution}) {child_task.desc}"
+
+        if not child_task.resolution:
+            # TODO: We might be able to just leave this open
+            raise ValueError(f"ERROR: Task #{t1.task_id} has child #{child_task.task_id} still open, must be resolved manually")
+
+        # Set or append the info string
+        tl_0 = draft_linkages[child_task_scopes[0]]
+        if not tl_0.detailed_resolution:
+            tl_0.detailed_resolution = info_string
+        else:
+            # If multiple child tasks existed, append info
+            if tl_0.detailed_resolution[0:2] != "- ":
+                tl_0.detailed_resolution = f"- {tl_0.detailed_resolution}"
+            tl_0.detailed_resolution = f"{tl_0.detailed_resolution}\n- {info_string}"
+
+        del info_string
+
+        # Update accessory fields, if we can
+        if should_populate_first_scope:
+            tl_0.created_at = child_task.created_at
+
+        if child_task.time_actual:
+            if tl_0.time_elapsed:
+                tl_0.time_elapsed += child_task.time_actual
+            else:
+                tl_0.time_elapsed = child_task.time_actual
+
+    def add_child_tasks(t1: Task_v1):
+        for child in t1.get_children():
+            # print(f"DEBUG: evaluating child #{child.task_id}")
+            parse_child(child)
+            add_child_tasks(child)
+
+    add_child_tasks(t1)
+
+    # Done with initial import pass, add additional info
+    draft_linkages_sorted = sorted(draft_linkages.items())
+    for index, (scope, tl) in enumerate(draft_linkages_sorted):
+        # attempt to fill in "roll =>" entries (skip the last entry in the set, though)
+        if index < len(draft_linkages_sorted) - 1:
+            if not tl.resolution:
+                tl.resolution = f"roll => {draft_linkages_sorted[index + 1][0]}"
+
+        # add "migrated from" note, where applicable
+        if not tl.detailed_resolution:
+            tl.detailed_resolution = f"migrated from Task_v1 #{t1.task_id}"
+    del index, scope, tl
+
+    # Sometimes child tasks have scopes that extend out past the parent
+    final_linkage = draft_linkages_sorted[-1][1]
+    if t1.resolution and not final_linkage.resolution:
+            print(f"WARN: task #{t1.task_id} imported, but final child scope {final_linkage.time_scope_id} is later than final parent scope {t1.first_scope}")
+    del final_linkage
+
+    return draft_linkages
+
+
 def _migrate_one(session, t1: Task_v1, print_fn):
-    print_fn(f"skipping {t1}")
-    return None, 0
+    """
+    Returns new parent task, plus count of total migrated tasks
+
+    Expected to return None on failure, not raise exceptions
+    """
+
+    def print_task_and_children(depth, t: Task_v1):
+        """
+        Recursive print. Also counts the number of prints.
+        """
+        print_fn("    " * depth + f"- #{t.task_id}: ({t.resolution}) {t.desc}")
+        print_fn("    " * depth + f"  scopes: {_get_scopes(t)}")
+        task_count = 1
+
+        for child in t.get_children():
+            task_count += print_task_and_children(depth + 1, child)
+
+        return task_count
+
+    # Print Task_v1 info
+    print_fn(f"importing #{t1.task_id}, old info:")
+    print_fn("-" * 72)
+    print_fn()
+    task_v1_count = print_task_and_children(0, t1)
+    print_fn()
+    print_fn()
+
+    # Check if Task_v2 already exists
+    existing_t2 = Task_v2.query \
+        .filter(Task_v2.desc == t1.desc) \
+        .one_or_none()
+    if not existing_t2:
+        # Construct Task_v2
+        try:
+            t2 = Task_v2(desc=t1.desc,
+                         category=t1.category,
+                         time_estimate=t1.time_estimate)
+            session.add(t2)
+            session.commit()
+        except StatementError as e:
+            session.rollback()
+            print(f"ERROR: Hit exception when parsing task #{t1.task_id}, skipping")
+            print(t1.to_json_dict())
+            return None, 0
+        except IntegrityError as e:
+            session.rollback()
+            print(e)
+            print(t1.to_json_dict())
+            return None, 0
+
+    print_fn("migrating to Task_v2, new info:")
+    print_fn("-" * 72)
+    print_fn()
+
+    if existing_t2:
+        print(f"WARN: #{t1.task_id} matches existing Task_v2 #{existing_t2.task_id}, reusing")
+        t2 = existing_t2
+
+    # Print Task_v2 info
+    print_fn(f"{t2.desc}")
+    print_fn()
+
+    try:
+        t2_linkages = _construct_linkages(t1, t2)
+    except ValueError as e:
+        print(e)
+        input(f"Import failed for task #{t1.task_id}, press enter to ignore bad import and continue")
+        return None, 0
+
+    for scope, tl in sorted(t2_linkages.items()):
+        print_fn(f"- {tl.time_scope_id}: {tl.resolution} / {tl.detailed_resolution}")
+        session.add(tl)
+
+    print_fn()
+    print_fn()
+
+    session.commit()
+    return t2, task_v1_count
 
 
 def migrate_tasks(tasks_v1_session,
