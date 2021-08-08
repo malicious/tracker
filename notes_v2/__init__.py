df394fae3420b57cee2b88aa23a56a46289cfddd
malicious <38064672+malicious@users.noreply.github.com>
2021-08-07 22:52:25 -0700

    n2: Load SQL models correctly
    Tests currently just load an in-memory SQLite database

--- notes_v2/__init__.py
+++ notes_v2/__init__.py
@@ -1,2 +1,22 @@
+import sqlalchemy
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from notes_v2.models import Base
 # noinspection PyUnresolvedReferences
 from . import models
+
+db_session = None
+
+
+def load_models(current_db_path: str):
+    engine = sqlalchemy.create_engine('sqlite:///' + current_db_path)
+
+    Base.metadata.create_all(bind=engine)
+
+    # Create a Session object and bind it to the declarative_base
+    global db_session
+    db_session = scoped_session(sessionmaker(autocommit=False,
+                                             autoflush=False,
+                                             bind=engine))
+
+    Base.query = db_session.query_property()
--- tests/notes_v2/test_n2.py
+++ tests/notes_v2/test_n2.py
@@ -4,3 +4,12 @@ from notes_v2.models import Note
 def test_note_constructor():
     note = Note()
     assert note
+
+
+def test_create(note_v2_session):
+    note = Note(time_scope_id="2020-ww20.4", desc="status update")
+    assert note.note_id is None
+
+    note_v2_session.add(note)
+    note_v2_session.flush()
+    assert note.note_id is not None
