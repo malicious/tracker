608e1b3a4953d3e35740046ae886bd4e14728664
malicious <38064672+malicious@users.noreply.github.com>
2023-10-22 16:24:42 +0000

    n2/render: render `/svg.week` using generator expressions
    Doesn't seem to be much faster than before, though.

--- notes_v2/report/render.py
+++ notes_v2/report/render.py
@@ -1,6 +1,7 @@
 import functools
 import hashlib
 from datetime import datetime, timedelta
+from typing import Iterable
 
 from flask import Response
 
@@ -84,17 +85,17 @@ def render_day_svg(day_scope, day_notes, svg_width=960) -> str:
     # finally, the overall day boundaries + text label(s)
     rendered_notes.extend([
         '<line '
-            f'x1="{svg_width * 1 / 4}" y1="{0.15 * height_factor:.3f}" '
-            f'x2="{svg_width * 1 / 4}" y2="{0.85 * height_factor:.3f}" stroke="black" />',
+        f'x1="{svg_width * 1 / 4}" y1="{0.15 * height_factor:.3f}" '
+        f'x2="{svg_width * 1 / 4}" y2="{0.85 * height_factor:.3f}" stroke="black" />',
         f'<text x="{svg_width / 2}" y="{0.85 * height_factor:.3f}" '
-            'text-anchor="middle" opacity="0.5" style="font-size: 12px">'
-            f'{(start_time + timedelta(hours=12)).strftime("%G-ww%V.%u-%b-%d")}</text>',
+        'text-anchor="middle" opacity="0.5" style="font-size: 12px">'
+        f'{(start_time + timedelta(hours=12)).strftime("%G-ww%V.%u-%b-%d")}</text>',
         '<line '
-            f'x1="{svg_width * 3 / 4}" y1="{0.15 * height_factor:.3f}" '
-            f'x2="{svg_width * 3 / 4}" y2="{0.85 * height_factor:.3f}" stroke="black" />',
+        f'x1="{svg_width * 3 / 4}" y1="{0.15 * height_factor:.3f}" '
+        f'x2="{svg_width * 3 / 4}" y2="{0.85 * height_factor:.3f}" stroke="black" />',
         f'<text x="{svg_width}" y="{0.85 * height_factor:.3f}" '
-            'text-anchor="end" opacity="0.5" style="font-size: 12px">'
-            f'{(start_time + timedelta(hours=36)).strftime("ww%V.%u")}</text>',
+        'text-anchor="end" opacity="0.5" style="font-size: 12px">'
+        f'{(start_time + timedelta(hours=36)).strftime("ww%V.%u")}</text>',
     ])
 
     return '''<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}" id="{}">{}</svg>'''.format(
@@ -120,7 +121,11 @@ def standalone_render_day_svg(db_session, day_scope, domains, disable_caching):
     return response
 
 
-def render_week_svg(week_scope, notes_dict) -> str:
+def render_week_svg(
+        week_scope_id: str,
+        notes_dict,
+        indent: int = 6,
+) -> str:
     """
     Render data vertically, like a weekly calendar.
     """
@@ -130,67 +135,62 @@ def render_week_svg(week_scope, notes_dict) -> str:
     col_width_and_right_margin = col_width + 4
     row_height = 20
 
-    rendered_notes = []
-
-    def draw_hour(column, row):
+    def _draw_hour_line(column, row) -> str:
         """
         This "weekly" svg is usually split into 9 columns and 24 rows.
 
         For ease, this is expected to be 90px x 20px.
         """
-        hour_line = (
-            '<line '
-            'x1="{:.3f}" y1="{}" '
-            'x2="{:.3f}" y2="{}" '
-            'stroke="black" opacity="0.1" />'
-        ).format(
-            column * col_width_and_right_margin + col_width/2 - 10,
-            row * row_height,
-            column * col_width_and_right_margin + col_width/2 + 10,
-            row * row_height
-        )
-
         # Draw the every-6-hours lines special
         if row % 6 == 0:
-            hour_line = '<line x1="{:.3f}" y1="{}" x2="{:.3f}" y2="{}" stroke="black" opacity="0.4" />'.format(
+            return '<line x1="{:.3f}" y1="{}" x2="{:.3f}" y2="{}" stroke="black" opacity="0.4" />'.format(
                 column * col_width_and_right_margin + col_width / 2 - 15,
                 row * row_height,
                 column * col_width_and_right_margin + col_width / 2 + 15,
-                row * row_height)
+                row * row_height
+            )
+        else:
+            return '<line x1="{:.3f}" y1="{}" x2="{:.3f}" y2="{}" stroke="black" opacity="0.1" />'.format(
+                column * col_width_and_right_margin + col_width / 2 - 10,
+                row * row_height,
+                column * col_width_and_right_margin + col_width / 2 + 10,
+                row * row_height
+            )
 
-        rendered_notes.append(hour_line)
+    def draw_hour_lines() -> Iterable[str]:
+        # render the pre-monday, if needed
+        for row in range(24 - hours_before, 24):
+            yield _draw_hour_line(column=0, row=row)
 
-    # render the pre-monday, if needed
-    for row in range(24 - hours_before, 24):
-        draw_hour(column=0, row=row)
+        # and now the "normal" days
+        for column in range(1, 8):
+            for row in range(1, 24):
+                yield _draw_hour_line(column, row)
 
-    # and now the "normal" days
-    for column in range(1, 8):
-        for row in range(1, 24):
-            draw_hour(column, row)
+        # and the after-sunday
+        for row in range(1, hours_after + 1):
+            yield _draw_hour_line(column=8, row=row)
 
-        day_label = (
-            '<text '
-            f'x="{column * col_width_and_right_margin + col_width/2}" y="{24 * row_height - 8}" '
-            'text-anchor="middle" opacity="0.5" style="font-size: 10px">'
-            f'{week_scope[5:] + "." + str(column)}'
-            '</text>'
-        )
-        rendered_notes.append(day_label)
-
-    # and the after-sunday
-    for row in range(1, hours_after + 1):
-        draw_hour(column=8, row=row)
+        # and text for the "normal" days
+        for column in range(1, 8):
+            day_label = (
+                '<text '
+                f'x="{column * col_width_and_right_margin + col_width / 2}" y="{24 * row_height - 8}" '
+                'text-anchor="middle" opacity="0.5" style="font-size: 10px">'
+                f'{week_scope_id[5:] + "." + str(column)}'
+                '</text>'
+            )
+            yield day_label
 
     # and the actual individual notes
-    def render_note_dot(note):
+    def _draw_note_dot(note) -> str | None:
         """
         TODO: This is hard-coded to expect that the SVG chart starts on previous Sunday.
         """
         if not hasattr(note, 'sort_time') or not note.sort_time:
-            return
+            return None
         if not TimeScope(note.time_scope_id).is_day():
-            return
+            return None
 
         column = note.sort_time.isoweekday()
         if column == 1 and note.time_scope_id[-1] == "7":
@@ -209,25 +209,29 @@ def render_week_svg(week_scope, notes_dict) -> str:
         dot_x_offset = ((note.sort_time - day_scope_time).total_seconds() % (60 * 60)) / (60 * 60)
         dot_x_offset = dot_radius + dot_x_offset * (col_width - 2 * dot_radius)
 
-        svg_element = '<circle cx="{:.3f}" cy="{:.3f}" r="{}" {} />'.format(
+        return '<circle cx="{:.3f}" cy="{:.3f}" r="{}" {} />'.format(
             column * col_width_and_right_margin + dot_x_offset,
             int((note.sort_time - day_scope_time).total_seconds() / (60 * 60)) * row_height + row_height / 2,
             dot_radius,
             f'style="fill: none; {dot_color}" tracker-note-id="{note.note_id}"')
-        rendered_notes.append(svg_element)
 
-    for day_scope, day_dict in notes_dict.items():
-        if day_scope == "notes":
-            for note in day_dict:
-                render_note_dot(note)
-        else:
-            for note in day_dict['notes']:
-                render_note_dot(note)
+    def draw_note_dots() -> Iterable[str]:
+        for day_scope, day_dict in notes_dict.items():
+            if day_scope == "notes":
+                for note in day_dict:
+                    yield _draw_note_dot(note)
+            else:
+                for note in day_dict['notes']:
+                    yield _draw_note_dot(note)
 
-    return '''<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}">\n  {}\n</svg>'''.format(
-        9 * col_width_and_right_margin,
-        24 * row_height,
-        '\n  '.join(rendered_notes))
+    return (
+            '<svg xmlns="http://www.w3.org/2000/svg" '
+            f'width="{9 * col_width_and_right_margin}" '
+            f'height="{24 * row_height}">\n'
+            + ('\n' + ' ' * indent).join(draw_hour_lines())
+            + ('\n' + ' ' * indent).join(dot for dot in draw_note_dots() if dot is not None)
+            + '\n' + '</svg>'
+    )
 
 
 def standalone_render_week_svg(db_session, week_scope, domains, disable_caching):
@@ -241,4 +245,3 @@ def standalone_render_week_svg(db_session, week_scope, domains, disable_caching)
     if not disable_caching:
         response.cache_control.max_age = 31536000
     return response
-
